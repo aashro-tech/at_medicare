f8767eb29a74a7aafb8cb5da0426d8834fc2be1a
Chirag Solanki <chirag.aashro@gmail.com>
2026-01-07 10:41:34 +0530

    Initial commit for TYPO3 Template

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,39 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
-    'title' => '[AASHRO] Medic Care TYPO3 Template',
-    'description' => 'A professional TYPO3 healthcare template with appointment booking, doctor profiles, timelines, and testimonials—ideal for clinics and medical services.',
-    'category' => 'templates',
-    'author' => 'Team AASHRO',
-    'author_email' => 'info@aashro.com',
-    'author_company' => 'AASHRO Tech',
-    'state' => 'stable',
-    'uploadfolder' => false,
-    'clearCacheOnLoad' => false,
-    'version' => '1.0.0',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '13.0.0-13.99.99',
-            'mask' => '9.0.0-9.0.9',
-        ],
-        'conflicts' => [],
-        'suggests' => [],
-    ],
-];
+/***************************************************************
+ * Extension Manager/Repository config file for ext "at_medicare".
+ *
+ * Auto generated 07-01-2026 09:59
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => '[AASHRO] Medic Care TYPO3 Template',
+  'description' => 'A professional TYPO3 healthcare template with appointment booking, doctor profiles, timelines, and testimonials—ideal for clinics and medical services.',
+  'category' => 'templates',
+  'version' => '1.0.1',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'clearcacheonload' => false,
+  'author' => 'Team AASHRO',
+  'author_email' => 'info@aashro.com',
+  'author_company' => 'AASHRO Tech',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '13.0.0-13.99.99',
+      'mask' => '9.0.0-9.0.9',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
+
